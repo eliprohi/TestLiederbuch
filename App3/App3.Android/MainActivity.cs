08724d911efb5d9fd5e30d021c914f19cb5d0c85
Elias Prohaszka <eliprohi@yahoo.de>
2019-10-23 14:19:11 +0200

    Mappe hinzugefügt

--- App3/App3.Android/MainActivity.cs
+++ App3/App3.Android/MainActivity.cs
@@ -9,7 +9,7 @@ using Android.OS;
 
 namespace App3.Droid
 {
-    [Activity(Label = "App3", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    [Activity(Label = "Songs", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
         protected override void OnCreate(Bundle savedInstanceState)
